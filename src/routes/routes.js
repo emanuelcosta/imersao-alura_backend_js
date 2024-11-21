b0cf7fc98cc4ccda57805bf8ff29d74860ecdfe1
Emanuel Costa <emanuelcosta.es@gmail.com>
2024-11-21 01:23:57 -0300

    commit aula 03

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import {listarPosts,findPostById} from "../controllers/postsController.js"
-
-const routes = (app) => {
-    app.get('/posts',listarPosts)
-    app.get('/posts/:id',findPostById)
-}
-
-export default routes;
